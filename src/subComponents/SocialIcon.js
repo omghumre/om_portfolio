548513d426e127723ca9a78d4b75824dfba34805
omg12347 <omghumre74@gmail.com>
2024-06-29 23:08:23 +0530

    styled socialicons

--- src/subComponents/SocialIcon.js
+++ src/subComponents/SocialIcon.js
@@ -1,9 +1,29 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import {Facebook, Github, Twitter, YouTube} from '../components/Allsvg'
+import styled from 'styled-components'
+
+const Icons = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    position: fixed;
+    bottom: 0;
+    left: 2rem;
+
+    z-index: 3;
+`
+const Line = styled.span`
+  width: 2px;
+  height: 8rem;
+  background-color: ${props => props.theme.text};
+
+`
+
 const SocialIcon = () => {
   return (
-    <div>
+    <Icons>
         <div>
             <NavLink to="/">
                 <Github width={30} height={30} fill='currentColor' />
@@ -24,8 +44,9 @@ const SocialIcon = () => {
                 <Facebook width={30} height={30} fill='currentColor' />
             </NavLink>
         </div>
-    </div>
-  )
+        <Line/>
+    </Icons>
+    )
 }
 
 export default SocialIcon
