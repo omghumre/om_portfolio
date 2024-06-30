95a1946c58bdede62d7da3d9ae1bc674044aa79a
omg12347 <omghumre74@gmail.com>
2024-07-01 00:04:37 +0530

    framer animation done in intro

--- src/components/intro.js
+++ src/components/intro.js
@@ -1,17 +1,19 @@
 import React from 'react'
 import styled from 'styled-components'
+import {motion} from 'framer-motion'
+import Me from '../assets/Images/profile-img.png'
 
 
-const Box = styled.div`
+const Box = styled(motion.div)`
     position: absolute;
   left: 50%;
   top: 50%;
   transform: translate(-50%, -50%);
-  width: 55vw;
-  height: 40vh;
+  width: 57vw;
+  height: 55vh;
+
   display: flex;
-  background-repeat: no-repeat;
-  background-size: 100% 2px;
+  
   background: linear-gradient(
     to right,
     ${props => props.theme.body} 50%,
@@ -22,15 +24,72 @@ const Box = styled.div`
     ${props => props.theme.body} 50%,
     ${props => props.theme.text} 50%
   ) top;
+  background-repeat: no-repeat;
+  background-size: 100% 2px;
   border-left: 2px solid ${props => props.theme.body};
   border-right: 2px solid ${props => props.theme.text};
+  
+
   z-index: 1;
+`
+
+const SubBox = styled.div`
+    width: 50%;
+    position: relative;
+    display: flex;
+
+    .pic {
+        position: absolute;
+        bottom: 0;
+        left: 50%;
+        transform: translate(-50%,0%);
+        width: 100%;
+        height: auto;
+    }
+`
+
+const Text = styled.div`
+    font-size: calc(1em + 1.5vw);
+    color: ${props => props.theme.body};
+    padding: 2rem;
+    cursor: pointer;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+
+    &>:last-child{
+      color: ${props => `rgba(${props.theme.bodyRgba},0.6)`};
+        font-size: calc(0.5rem + 1.5vw);
+        font-weight:300;
+  }
 `
 
 const Intro = () => {
   return (
-    <Box>
-        intro
+    <Box
+      initial = {{height:0}}
+      animate={{height:'55vh'}}
+      transition={{type: 'spring', duration:2, delay:1 }}
+    >
+        <SubBox>
+          <Text>
+            <h1>Hi,</h1>
+            <h3>I'm Om Ghumre.</h3>
+            <h3>
+            <h3>Machine learning Enthusiast and</h3>
+            <h3>Frontend Developer.</h3>
+            </h3>
+          </Text>
+        </SubBox>
+        <SubBox>
+          <motion.div
+          initial = {{opacity:0}}
+          animate={{opacity:1}}
+          transition={{duration:1, delay:2 }}
+          >
+            <img className='pic' src={Me} alt="Profile Pic"></img>
+          </motion.div>
+        </SubBox>
     </Box>
   )
 }
