252cf91b4853cd7a8da4d761f85cd62b31e7466b
omg12347 <omghumre74@gmail.com>
2024-06-30 19:13:46 +0530

    intro box done

--- src/components/intro.js
+++ src/components/intro.js
@@ -1,10 +1,37 @@
 import React from 'react'
+import styled from 'styled-components'
+
+
+const Box = styled.div`
+    position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  width: 55vw;
+  height: 40vh;
+  display: flex;
+  background-repeat: no-repeat;
+  background-size: 100% 2px;
+  background: linear-gradient(
+    to right,
+    ${props => props.theme.body} 50%,
+    ${props => props.theme.text} 50%
+  ) bottom,
+  linear-gradient(
+    to right,
+    ${props => props.theme.body} 50%,
+    ${props => props.theme.text} 50%
+  ) top;
+  border-left: 2px solid ${props => props.theme.body};
+  border-right: 2px solid ${props => props.theme.text};
+  z-index: 1;
+`
 
 const Intro = () => {
   return (
-    <div>
+    <Box>
         intro
-    </div>
+    </Box>
   )
 }
 
