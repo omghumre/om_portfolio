c08ef53d4db4492de30626c3b4c86dc30987c6b1
omg12347 <omghumre74@gmail.com>
2024-07-01 12:58:29 +0530

    added style to blog box

--- src/components/BlogComponent.js
+++ src/components/BlogComponent.js
@@ -8,6 +8,11 @@ const Box = styled(NavLink)`
     text-decoration: none;
     height: 20rem;
     padding: 1rem;
+    color: ${props => props.theme.text};
+    border: 2px solid ${props => props.theme.text};
+    backdrop-filter: blur(4px);
+    box-shadow: 0 0 1rem 0 rgba(0,0,0,0.2);
+    cursor: pointer;
 `
 
 const Image = styled.div`
@@ -19,7 +24,14 @@ const Image = styled.div`
     background-position: center center;
 }
 `
-
+const Title = styled.h3`
+    color: inherit;
+    padding: 0.5rem 0;
+    padding-top: 1rem;
+    font-family: 'Karla',sans-serif;
+    font-weight:700;    
+    border-bottom: 1px solid ${props => props.theme.text};
+`
 
 const BlogComponent = (props) => {
 
@@ -28,6 +40,7 @@ const BlogComponent = (props) => {
     return (
     <Box target="_blank"  to={{pathname: link}}>
         <Image img={imgSrc} /> 
+        <Title>{name}</Title>
     </Box>
     )
 }
