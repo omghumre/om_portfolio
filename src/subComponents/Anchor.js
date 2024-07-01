deaa68d0d711fb2c4464e5035808af9d99d6c172
omg12347 <omghumre74@gmail.com>
2024-07-01 21:31:01 +0530

    making the chain scroll

--- src/subComponents/Anchor.js
+++ src/subComponents/Anchor.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react'
+import React, { useEffect, useRef } from 'react'
 import styled from 'styled-components'
 import { Anchor, chain } from '../components/Allsvg'
 import { Link } from '../components/Allsvg'
@@ -11,17 +11,62 @@ const Slider = styled.div`
     position: fixed;
     top: 0;
     right: 2rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    transform: translateY(-100%)
+
+    .chain{
+        transform: rotate(135deg);
+    }
 
 `
-
+const PreDisplay = styled.div`
+    position: absolute;
+    top:0;
+    right: 2rem;
+`
 
 const AnchorComponent = () => {
 
     const ref = useRef(null); 
     const hiddenRef = useRef(null); 
 
+
+    useEffect(() => {
+        
+        const handleScroll = () => {
+            let scrollPosition = window.pageYOffset;
+            let windowSize = window.innerHeight;
+            let bodyHeight = document.body.offsetHeight;
+
+            let diff = Math.max(bodyHeight - (scrollPosition + windowSize));
+
+            let diffP = (diff * 100) / (bodyHeight - windowSize);
+
+            ref.current.style.transform = `translateY(${-diffP}%)`
+
+            if(window.pageYOffset > 5){
+                hiddenRef.current.style.display = 'none'
+            }else{
+                hiddenRef.current.style.display = 'block'
+            }
+        }
+
+        window.addEventListener('scroll', handleScroll)
+
+        return () => window.removeEventListener('scroll', handleScroll)
+
+    }, []);
+
     return (
     <Container>
+
+        <PreDisplay ref={hiddenRef} className="hidden">
+            <Anchor width={70} height={70} fill='currentColor'  />
+        </PreDisplay>
+
         <Slider ref = {ref}>
             {
                 [...Array(25)].map((x,id) => {
