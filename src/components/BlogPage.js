aedee7467ba45b8785bbb19ee41dc5fe4718e3a8
omg12347 <omghumre74@gmail.com>
2024-07-01 12:07:28 +0530

    started blog page

--- src/components/BlogPage.js
+++ src/components/BlogPage.js
@@ -1,10 +1,37 @@
 import React from 'react'
+import styled from 'styled-components'
+
+import img from '../assets/Images/patrick-tomasso-Oaqk7qqNh_c-unsplash.jpg'
+
+const MainContainer = styled.div`
+    background-image: url(${img}) ;
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    background-position: center;
+    width: 100vw;
+    
+    
+`
+
+const Container = styled.div`
+  background-color: ${props => `rgba(${props.theme.bodyRgba},0.8)`}; 
+  width: 100%;
+  height: 100vh;
+  position: relative;
+  padding-bottom: 5rem;
+`
 
 const BlogPage = () => {
   return (
-    <div>
-      BlogPage Component/Page
-    </div>
+    <MainContainer>
+      <Container>
+        <LogoComponent />
+        <PowerButton />
+        <SocialIcon/>
+
+      </Container>
+    </MainContainer>
   )
 }
 
