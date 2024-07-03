ad8dba83c2c213703e572ad2baaffd63135c9984
omg12347 <omghumre74@gmail.com>
2024-07-03 14:30:27 +0530

    updated skills page

--- src/components/MySkillsPage.js
+++ src/components/MySkillsPage.js
@@ -1,9 +1,32 @@
 import React from 'react'
 import styled, { ThemeProvider } from 'styled-components'
-
+import {lightTheme } from '../components/Themes'
+import {Design, Develope} from './Allsvg'
 
 const Box=styled.div`
-  
+    background-color: ${props => props.theme.body};
+    width: 100%;
+    height: 100vh;
+    position: relative;
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+`
+
+const Main = styled.div`
+    border: 2px solid ${props => props.theme.text};
+    color: ${props => props.theme.text};
+    background-color: ${props => props.theme.body};
+    padding: 2rem;
+    width: 30vw;
+    height: 60vh;
+    z-index: 3;
+    line-height: 1.5;
+
+    font-family: 'Ubuntu Mono', monospace ;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
 `
 
 const MySkillsPage = () => {
@@ -11,7 +34,13 @@ const MySkillsPage = () => {
 
     <ThemeProvider theme={lightTheme}> 
     <Box>
-      MySkillsPage Component/Page
+      <Main>
+    1
+      </Main>
+      <Main>
+    2
+      </Main>
+   
     </Box>
     </ThemeProvider>
 
