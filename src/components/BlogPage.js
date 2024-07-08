6ea46afaf21e03ab21397106b6eab56b5d64624a
omg12347 <omghumre74@gmail.com>
2024-07-08 22:51:55 +0530

    blogs update

--- src/components/BlogPage.js
+++ src/components/BlogPage.js
@@ -9,8 +9,10 @@ import {Blogs} from '../data/BlogData'
 import BlogComponent from './BlogComponent'
 import AnchorComponent from '../subComponents/Anchor'
 import BigTitle from '../subComponents/BigTitle'
+import { Opacity } from '@tsparticles/engine'
+import { motion } from 'framer-motion'
 
-const MainContainer = styled.div`
+const MainContainer = styled(motion.div)`
     background-image: url(${img}) ;
     background-size: cover;
     background-repeat: no-repeat;
@@ -26,6 +28,19 @@ const Container = styled.div`
   position: relative;
   padding-bottom: 5rem;
 `
+
+const container = {
+  hidden: {Opacity:0},
+  show: {
+    opacity:1,
+
+    transition:{
+      staggerChildren: 0.5,
+      duration: 0.5,
+    }
+  }
+}
+
 
 const Center = styled.div`
     display: flex;
@@ -52,7 +67,14 @@ const BlogPage = () => {
   }, []);
 
   return (
-    <MainContainer>
+    <MainContainer 
+      variants={container}
+      initial='hidden'
+      animate='show'
+      exit={{
+        opacity: 0, transition:{duration: 0.5}
+      }}
+    >
       <Container>
         <LogoComponent />
         <PowerButton />
