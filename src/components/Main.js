ab2d9a0fc3e8a639c01e0fdbf26a146df35bbedb
omg12347 <omghumre74@gmail.com>
2024-07-10 21:49:25 +0530

    making main page responsive

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,14 +1,12 @@
-import React, { useState } from 'react'
-import styled, { keyframes } from 'styled-components'
-import PowerButton from '../subComponents/PowerButton'
-import LogoComponent from '../subComponents/LogoComponent'
-import SocialIcon from '../subComponents/SocialIcon'
-import { NavLink } from 'react-router-dom'
-import {motion} from 'framer-motion'
-import { YinYang } from './Allsvg'
-import Intro from './intro'
-// import auroraComponent from '../subComponents/auroraComponent'
-
+import React, { useState } from 'react';
+import styled, { keyframes } from 'styled-components';
+import PowerButton from '../subComponents/PowerButton';
+import LogoComponent from '../subComponents/LogoComponent';
+import SocialIcon from '../subComponents/SocialIcon';
+import { NavLink } from 'react-router-dom';
+import { motion } from 'framer-motion';
+import { YinYang } from './Allsvg';
+import Intro from './intro';
 
 const MainContainer = styled.div`
   background: ${props => props.theme.body};
@@ -17,44 +15,41 @@ const MainContainer = styled.div`
   overflow: hidden;
   position: relative;
 
-  h2,h3,h4,h5,h6 {
+  h2, h3, h4, h5, h6 {
     font-family: 'Karla', sans-serif;
-    font-weight:500;
-    
+    font-weight: 500;
   }
 
   h2 {
     font-family: 'Montserrat', sans-serif;
-    font-weight:500;
-    margin: 0; /* Reset default margin */
-    font-size: 1.2rem; /* Adjust font size as needed */
-    font-weight:600;
+    font-weight: 500;
+    margin: 0;
+    font-size: 1.2rem;
+    font-weight: 600;
   }
 
   h6 {
-  font-family: 'Montserrat', sans-serif;
-  font-weight: 300;
-  font-style: normal;
+    font-family: 'Montserrat', sans-serif;
+    font-weight: 300;
+    font-style: normal;
     font-size: 1.8rem;
   }
 
-  @media screen and (max-width: 700px){
+  @media screen and (max-width: 700px) {
     h2 {
       font-size: 1rem;
     }
 
-    h6{
+    h6 {
       font-size: 1.6rem;
     }
 
-    h1{
+    h1 {
       font-size: 1.6rem;
       transform: translateY(5px);
     }
   }
-`
-
-
+`;
 
 const Container = styled.div`
   padding: 0rem;
@@ -64,331 +59,314 @@ const Container = styled.div`
   justify-content: center;
   width: 100%;
   height: 100%;
+`;
 
-`
 const Contact = styled(NavLink)`
   color: ${props => props.theme.text};
-  position:absolute;
+  position: absolute;
   top: 2rem;
   right: 3vw;
   text-decoration: none;
   z-index: 1;
   font-size: 40px;
-  @media screen and (max-width: 700px){
-      font-size: 20px;
+  @media screen and (max-width: 700px) {
+    font-size: 20px;
   }
-`
+`;
 
 const BLOG = styled(NavLink)`
   color: ${props => props.theme.text};
-  position:absolute;
+  position: absolute;
   top: 50%;
-  transform: rotate(90deg) translate(-50%,-50%);
+  transform: rotate(90deg) translate(-50%, -50%);
   right: 3vw;
   text-decoration: none;
-  z-index: 1; 
-  
-`
+  z-index: 1;
+`;
 
 const WORK = styled(NavLink)`
-  color: ${props => props.click ? props.theme.body : props.theme.text};
-  position:absolute;
+  color: ${props => (props.click ? props.theme.body : props.theme.text)};
+  position: absolute;
   top: 50%;
-  transform: translate(-50%,-50%) rotate(-90deg) ;
-  left  : 3rem;
+  transform: translate(-50%, -50%) rotate(-90deg);
+  left: 3rem;
   text-decoration: none;
   z-index: 1;
 
-  @media screen and (max-width: 700px){
-      transform: translate(-50%,-20px) rotate(-90deg) ;
-      
-  } 
-`
+  @media screen and (max-width: 700px) {
+    transform: translate(-50%, -20px) rotate(-90deg);
+  }
+`;
 
 const BottomBar = styled.div`
-    position: absolute;
-    bottom: 1rem;
-    left: 0;
-    right: 0;
-    width: 100%;
-    display: flex;
-    justify-content: space-evenly;
-    align-items: center;
-    
-    
-`
+  position: absolute;
+  bottom: 1rem;
+  left: 0;
+  right: 0;
+  width: 100%;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+`;
 
 const ABOUT = styled(NavLink)`
-  color: ${props => props.click ? props.theme.body : props.theme.text};
+  color: ${props => (props.click ? props.theme.body : props.theme.text)};
   text-decoration: none;
-  z-index: 1; 
-  @media screen and (max-width: 700px){
-      transform: rotate(-90deg) ;
+  z-index: 1;
+  @media screen and (max-width: 700px) {
+    transform: rotate(-90deg);
   }
-`
+`;
 
 const SKILLS = styled(NavLink)`
   color: ${props => props.theme.text};
   text-decoration: none;
-  z-index: 1; 
-  @media screen and (max-width: 700px){
-      transform: translateX(15px) rotate(90deg) ;
+  z-index: 1;
+  @media screen and (max-width: 700px) {
+    transform: translateX(15px) rotate(90deg);
   }
-`
+`;
 
 const TopBar = styled.div`
-    position: absolute;
-    top: 2.2rem;
-    left: 0;
-    right: 0;
-    width: 100%;
-    display: flex;
-    justify-content: space-evenly;
-    align-items: center;
-    
-    @media screen and (max-width: 700px){
-      transform: translate(0,20vh);
-      gap: 60vw;
-    }
-`
+  position: absolute;
+  top: 2.2rem;
+  left: 0;
+  right: 0;
+  width: 100%;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+
+  @media screen and (max-width: 700px) {
+    transform: translate(0, 20vh);
+    gap: 60vw;
+  }
+`;
+
 const CERTIFICATES = styled(NavLink)`
-  color: ${props => props.click ? props.theme.body : props.theme.text};
+  color: ${props => (props.click ? props.theme.body : props.theme.text)};
   text-decoration: none;
-  z-index: 1; 
-`
+  z-index: 1;
+`;
 
 const PROJECTS = styled(NavLink)`
   color: ${props => props.theme.text};
   text-decoration: none;
-  z-index: 1; 
-`
+  z-index: 1;
+`;
 
 const rotate = keyframes`
-  from{
+  from {
     transform: rotate(0);
   }
-  to{
+  to {
     transform: rotate(360deg);
   }
-`
+`;
 
 const Center = styled.button`
-    position: absolute;
-    top: ${props => props.click ? '85%' : '50%' };
-    left: ${props => props.click ? '92%' : '50%' };
-    transform: translate(-50%,-50%);
-    border: none;
-    outline: none;
-    background-color: transparent;
-    cursor: pointer;
-    
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items:center;
+  position: absolute;
+  top: ${props => (props.click ? '85%' : '50%')};
+  left: ${props => (props.click ? '92%' : '50%')};
+  transform: translate(-50%, -50%);
+  border: none;
+  outline: none;
+  background-color: transparent;
+  cursor: pointer;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  transition: all 1s ease;
+
+  & > :first-child {
+    animation: ${rotate} infinite 1.5s linear;
+  }
+
+  & > :last-child {
+    display: ${props => (props.click ? 'none' : 'inline-block')};
+    padding-top: 1rem;
+  }
+
+  @media screen and (max-width: 700px) {
+    transform: translate(-50%, -50%);
+    top: ${props => (props.click ? '85%' : '50%')};
     transition: all 1s ease;
-
-    &>:first-child{
-      animation: ${rotate} infinite 1.5s linear
-    }
-
-    &>:last-child{
-      display: ${props => props.click ? 'none' : 'inline-block' };
-      padding-top: 1rem;
-    }
-    
-    @media screen and (max-width: 700px){
-        
-      transform: translate(-50%,-50%) ;
-      height: 120px;
-      width: 120px;
-    }
-`
+  }
+`;
 
 const DarkDiv = styled.div`
-    position: absolute;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  background-color: #000;
+  right: 50%;
+  width: ${props => (props.click ? '50%' : '0%')};
+  height: ${props => (props.click ? '100%' : '0%')};
+  z-index: 1;
+  transition: height 0.5s ease, width 1s ease 0.5s;
+
+  @media screen and (max-width: 768px) {
     top: 0;
-    bottom: 0;
-    background-color: #000;
-    right: 50%;
-    width: ${props => props.click ? '50%' : '0%' };
-    height: ${props => props.click ? '100%' : '0%' };
-    z-index: 1;
-    transition: height 0.5s ease, width 1s ease 0.5s;
-    
-    @media screen and (max-width: 700px){
-        width: ${props => props.click ? '100%' : '0%' };
-        height: ${props => props.click ? '100%' : '0%' };
-    }
-`
+    bottom: auto;
+    height: ${props => (props.click ? '50%' : '0%')};
+    width: 100%;
+    right: 0;
+  }
+`;
+
 
 const Main = () => {
-
   const [click, setClick] = useState(false);
 
   const handleClick = () => setClick(!click);
 
   return (
     <MainContainer>
-      
-      <DarkDiv click={click}/>
-
+      <DarkDiv click={click} />
       <Container>
-      
-      <PowerButton/>
-      <LogoComponent theme = {click ? 'dark' : 'light'}/>
-      <SocialIcon theme = {click ? 'dark' : 'light'}/>
+        <PowerButton />
+        <LogoComponent theme={click ? 'dark' : 'light'} />
+        <SocialIcon theme={click ? 'dark' : 'light'} />
 
-      
-      <Center click={click}>
-        <YinYang onClick={() => handleClick()}  width={click ? 120:200} height={click ? 120:200} fill='currentColor' />
-        <span>Click here</span>
-      </Center>
+        <Center click={click}>
+          <YinYang
+            onClick={() => handleClick()}
+            width={click ? 120 : 200}
+            height={click ? 120 : 200}
+            fill='currentColor'
+          />
+          <span>Click here</span>
+        </Center>
 
-      <Contact target="_blank" to="mailto:omghumre74@gmail.com">
-        <motion.h6
+        <Contact target="_blank" to="mailto:omghumre74@gmail.com">
+          <motion.h6
+            initial={{
+              y: -200,
+              transition: { type: 'spring', duration: 1.5, delay: 1 }
+            }}
+            animate={{
+              y: 0,
+              transition: { type: 'spring', duration: 1.5, delay: 1 }
+            }}
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+          >
+            Hello..
+          </motion.h6>
+        </Contact>
 
-          initial={{
-            y:-200,
-            transition: {type:'spring', duration: 1.5, delay:1}
-          }}
-          animate={{
-            y:0,
-            transition: {type:'spring', duration: 1.5, delay:1}
-          }}
+        <BLOG to="/blog">
+          <motion.h2
+            initial={{
+              y: -200,
+              transition: { type: 'spring', duration: 1.5, delay: 1.1 }
+            }}
+            animate={{
+              y: 0,
+              transition: { type: 'spring', duration: 1.5, delay: 1.1 }
+            }}
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+          >
+            Blogs
+          </motion.h2>
+        </BLOG>
 
-          whileHover={{scale:1.1}}
-          whileTap={{scale:0.9}}
-        >
-          Hello..
-        </motion.h6>
-      </Contact>
+        <WORK click={click} to="/work">
+          <motion.h2
+            initial={{
+              y: -200,
+              transition: { type: 'spring', duration: 1.5, delay: 1.2 }
+            }}
+            animate={{
+              y: 0,
+              transition: { type: 'spring', duration: 1.5, delay: 1.2 }
+            }}
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+          >
+            Work
+          </motion.h2>
+        </WORK>
 
-      <BLOG to="/blog">
-      <motion.h2
+        <TopBar>
+          <ABOUT click={click} to="/about">
+            <motion.h2
+              initial={{
+                y: -200,
+                transition: { type: 'spring', duration: 1.5, delay: 1.3 }
+              }}
+              animate={{
+                y: 0,
+                transition: { type: 'spring', duration: 1.5, delay: 1.3 }
+              }}
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }}
+            >
+              About.
+            </motion.h2>
+          </ABOUT>
 
-initial={{
-  y:-200,
-  transition: {type:'spring', duration: 1.5, delay:1.1}
-}}
-animate={{
-  y:0,
-  transition: {type:'spring', duration: 1.5, delay:1.1}
-}}
+          <SKILLS to="/skills">
+            <motion.h2
+              initial={{
+                y: -200,
+                transition: { type: 'spring', duration: 1.5, delay: 1.4 }
+              }}
+              animate={{
+                y: 0,
+                transition: { type: 'spring', duration: 1.5, delay: 1.4 }
+              }}
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }}
+            >
+              Skills.
+            </motion.h2>
+          </SKILLS>
+        </TopBar>
 
-          whileHover={{scale:1.1}}
-          whileTap={{scale:0.9}}
-        >
-          Blogs
-        </motion.h2>
-      </BLOG>
+        <BottomBar>
+          <CERTIFICATES click={click} to="/certificates">
+            <motion.h2
+              initial={{
+                y: 200,
+                transition: { type: 'spring', duration: 1.5, delay: 1.5 }
+              }}
+              animate={{
+                y: 0,
+                transition: { type: 'spring', duration: 1.5, delay: 1.5 }
+              }}
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }}
+            >
+              Certificates.
+            </motion.h2>
+          </CERTIFICATES>
 
-      <WORK click={click} to="/work">
-      <motion.h2
-
-initial={{
-  y:-200,
-  transition: {type:'spring', duration: 1.5, delay:1.2}
-}}
-animate={{
-  y:0,
-  transition: {type:'spring', duration: 1.5, delay:1.2}
-}}
-
-          whileHover={{scale:1.1}}
-          whileTap={{scale:0.9}}
-        >
-          Work
-        </motion.h2>
-      </WORK>
-
-      <TopBar>
-      <ABOUT click={click} to="/about">
-      <motion.h2
-
-initial={{
-  y:-200,
-  transition: {type:'spring', duration: 1.5, delay:1.3}
-}}
-animate={{
-  y:0,
-  transition: {type:'spring', duration: 1.5, delay:1.3}
-}}
-
-          whileHover={{scale:1.1}}
-          whileTap={{scale:0.9}}
-        >
-          About.
-        </motion.h2>
-      </ABOUT>
-      
-      <SKILLS to="/skills">
-      <motion.h2
-
-initial={{
-  y:-200,
-  transition: {type:'spring', duration: 1.5, delay:1.4}
-}}
-animate={{
-  y:0,
-  transition: {type:'spring', duration: 1.5, delay:1.4}
-}}
-
-          whileHover={{scale:1.1}}
-          whileTap={{scale:0.9}}
-        >
-          Skills.
-        </motion.h2>
-      </SKILLS>
-
-      
-      </TopBar>
-
-      <BottomBar>
-      <CERTIFICATES click={click} to="/certificates">
-      <motion.h2
-
-initial={{
-  y:200,
-  transition: {type:'spring', duration: 1.5, delay:1.5}
-}}
-animate={{
-  y:0,
-  transition: {type:'spring', duration: 1.5, delay:1.5}
-}}
-
-          whileHover={{scale:1.1}}
-          whileTap={{scale:0.9}}
-        >
-          Certificates.
-        </motion.h2>
-      </CERTIFICATES>
-      
-      <PROJECTS to="/projects">
-      <motion.h2
-
-initial={{
-  y:200,
-  transition: {type:'spring', duration: 1.5, delay:1.6}
-}}
-animate={{
-  y:0,
-  transition: {type:'spring', duration: 1.5, delay:1.6}
-}}
-
-          whileHover={{scale:1.1}}
-          whileTap={{scale:0.9}}
-        >
-          Projects.
-        </motion.h2>
-      </PROJECTS>
-      </BottomBar>
-
+          <PROJECTS to="/projects">
+            <motion.h2
+              initial={{
+                y: 200,
+                transition: { type: 'spring', duration: 1.5, delay: 1.6 }
+              }}
+              animate={{
+                y: 0,
+                transition: { type: 'spring', duration: 1.5, delay: 1.6 }
+              }}
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }}
+            >
+              Projects.
+            </motion.h2>
+          </PROJECTS>
+        </BottomBar>
       </Container>
 
-      {click ? <Intro click={click}/> : null}
+      {click ? <Intro click={click} /> : null}
     </MainContainer>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
--- src/components/intro.js
+++ src/components/intro.js
@@ -1,12 +1,10 @@
-import React from 'react'
-import styled from 'styled-components'
-import {motion} from 'framer-motion'
-import Me from '../assets/Images/profile.png'
-import { Omg } from './Allsvg'
-
+import React from 'react';
+import styled from 'styled-components';
+import { motion } from 'framer-motion';
+import Me from '../assets/Images/profile.png';
 
 const Box = styled(motion.div)`
-    position: absolute;
+  position: absolute;
   left: 50%;
   top: 50%;
   transform: translate(-50%, -50%);
@@ -14,7 +12,7 @@ const Box = styled(motion.div)`
   height: 55vh;
 
   display: flex;
-  
+
   background: linear-gradient(
     to right,
     ${props => props.theme.body} 50%,
@@ -29,72 +27,87 @@ const Box = styled(motion.div)`
   background-size: 100% 2px;
   border-left: 2px solid ${props => props.theme.body};
   border-right: 2px solid ${props => props.theme.text};
-  
 
   z-index: 1;
-`
+
+  @media screen and (max-width: 768px) {
+    flex-direction: column;
+    background: none;
+    border: none;
+    width: 80vw;
+    height: auto;
+  }
+`;
 
 const SubBox = styled.div`
-    width: 50%;
-    position: relative;
-    display: flex;
+  width: 50%;
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 
-    .pic {
-        position: absolute;
-        bottom: 2px;
-        left: 50%;
-        transform: translate(-50%,0%);
-        width: auto;
-        height: 25rem;
-        overflow: hidden;
+  .pic {
+    width: auto;
+    height: 25rem;
+    overflow: hidden;
+
+    @media screen and (max-width: 768px) {
+      height: 15rem;
     }
-`
+  }
+
+  @media screen and (max-width: 768px) {
+    width: 100%;
+  }
+`;
 
 const Text = styled.div`
-    font-size: calc(1em + 1.5vw);
-    color: ${props => props.theme.body};
-    padding: 2rem;
-    cursor: pointer;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-evenly;
+  font-size: calc(1em + 1.5vw);
+  color: ${props => props.theme.body};
+  padding: 2rem;
+  cursor: pointer;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
 
-    &>:last-child{
-      color: ${props => `rgba(${props.theme.bodyRgba},0.6)`};
-        font-size: calc(0.5rem + 1.5vw);
-        font-weight:300;
+  & > :last-child {
+    color: ${props => `rgba(${props.theme.bodyRgba}, 0.6)`};
+    font-size: calc(0.5rem + 1.5vw);
+    font-weight: 300;
   }
-`
+
+  @media screen and (max-width: 768px) {
+    text-align: center;
+    padding: 1rem;
+  }
+`;
 
 const Intro = () => {
   return (
     <Box
-      initial = {{height:0}}
-      animate={{height:'55vh'}}
-      transition={{type: 'spring', duration:2, delay:1 }}
+      initial={{ height: 0 }}
+      animate={{ height: '55vh' }}
+      transition={{ type: 'spring', duration: 2, delay: 1 }}
     >
-        <SubBox>
-          <Text>
-            <h1>Hi,</h1>
-            <h3>I'm Om Ghumre.</h3>
-            <h3>
-            <h3>Machine learning Enthusiast and</h3>
-            <h3>Frontend Developer.</h3>
-            </h3>
-          </Text>
-        </SubBox>
-        <SubBox>
-          <motion.div
-          initial = {{opacity:0}}
-          animate={{opacity:1}}
-          transition={{duration:1, delay:2 }}
-          >
-            {/* <Omg className='pic'/> */}
-            <img className='pic' src={Me} alt="Profile Pic"></img>
-          </motion.div>
-        </SubBox>
+      <SubBox>
+        <Text>
+          <h1>Hi,</h1>
+          <h3>I'm Om Ghumre.</h3>
+          <h3>Machine Learning Enthusiast and</h3>
+          <h3>Frontend Developer.</h3>
+        </Text>
+      </SubBox>
+      <SubBox>
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1, delay: 2 }}
+        >
+          <img className="pic" src={Me} alt="Profile Pic" />
+        </motion.div>
+      </SubBox>
     </Box>
-  )
-}
+  );
+};
 
-export default Intro
+export default Intro;
