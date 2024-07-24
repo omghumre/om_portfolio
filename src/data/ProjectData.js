3a70da15b20da021e5c87504723b341f0aac999c
omg12347 <omghumre74@gmail.com>
2024-07-24 23:42:55 +0530

    home button

--- src/data/ProjectData.js
+++ src/data/ProjectData.js
@@ -6,7 +6,6 @@ export const Work = [
         name:"JobSync: Company Recommendation Platform",
         description:"This project is a company recommendation platform developed during the HackNitr hackathon. The platform assists users in finding suitable companies to apply to based on the content of their resumes. ",
         tags:["NLTK","ML","Pandas","Numpy","Joblib","Streamlit","Hackthon-Finalist","HackNITR"],
-        // tags:"NLTK ML Pandss dhs hduish dfhsjf fhsndfh fjdbh  fidsf dhfsh hdsh  fbhdbf fdfhbs dfhsdb fjdbfh jdfshdjks" ,
         imgSrc:"./Project-Images/jobsync.png",
         demo:"https://devfolio.co/projects/jobsync-5ed7",
         github:"https://github.com/omghumre/JobSync"
